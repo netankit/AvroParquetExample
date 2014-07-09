04e50b0d267006943c1e8268a26740b928468011
netankit <ankit.bahuguna@cs.tum.edu>
2014-07-09 18:34:17 +0200

    Added all_names.txt and all_colors.txt

---
+++ src/main/java/com/teradata/avro/sample.java
@@ -0,0 +1,26 @@
+package com.teradata.avro;
+
+import java.util.Random;
+
+public class sample {
+
+	public static void main(String args[]) {
+		System.out.println(getRandomNumber());
+	}
+
+	private static Integer getRandomNumber() {
+		int min = 0;
+		int max = 2500000;
+
+		// Usually this should be a field rather than a method variable so
+		// that it is not re-seeded every call.
+		Random rand = new Random();
+
+		// nextInt is normally exclusive of the top value,
+		// so add 1 to make it inclusive
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+
+		return randomNum;
+	}
+
+}
