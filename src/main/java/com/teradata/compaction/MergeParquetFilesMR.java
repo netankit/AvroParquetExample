ac9853b3280f0d70b37ac146703650c5864582c6
netankit <ankit.bahuguna@cs.tum.edu>
2014-08-08 15:47:56 +0200

    Aded code for Compaction of ParquetFiles, which merge various parquet files in a given folder and generate one unique parquet file with and without using mapreduce

---
+++ src/main/java/com/teradata/compaction/MergeParquetFilesMR.java
@@ -0,0 +1,16 @@
+package com.teradata.compaction;
+
+/**
+ * This script can be used to merge all Parquet files present in a given
+ * directory, corresponding to a given schema, using map-reduce
+ * 
+ * @author ankit
+ *
+ */
+public class MergeParquetFilesMR {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/java/com/teradata/compaction/SimpleMergeParquetFiles.java
@@ -0,0 +1,16 @@
+package com.teradata.compaction;
+
+/**
+ * This script can be used to merge all Parquet files present in a given
+ * directory, corresponding to a given schema, without using map-reduce.
+ * 
+ * @author ankit
+ *
+ */
+public class SimpleMergeParquetFiles {
+
+	public static void main(String[] args) {
+
+	}
+
+}
