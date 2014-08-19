74ecff9344acb24d7a5156cfd10c5f15f6c788e0
netankit <ankit.bahuguna@cs.tum.edu>
2014-08-19 19:37:51 +0200

    Made small changes to GenericParquetMerge.java still unfinshed!

--- src/main/java/com/teradata/compaction/GenericParquetMerge.java
+++ src/main/java/com/teradata/compaction/GenericParquetMerge.java
@@ -18,52 +18,50 @@ import parquet.hadoop.metadata.CompressionCodecName;
 /**
  * This script can be used to merge all Parquet files present in a given
  * directory, irrespective of a given schema, without using map-reduce.This
- * script differs from SimpleMergePArquetFiles as it doesn't require an already
- * compile Avro object in order to make it more generic to unknown Avro objects.
+ * script differs from SimpleMergeParquetFiles as it doesn't require an already
+ * compiled Avro object in order to make it more generic to unknown Avro
+ * objects.
  * 
  * @author ankit
  *
  */
 
 public class GenericParquetMerge {
+	static ParquetWriter<GenericRecord> writer;
+
 	public static void main(String[] args) throws IOException {
 
-		String directory_path = "BulkParquetFiles";
+		String directory_path = "BulkParquetFiles1";
 		final File folder = new File(directory_path);
+		String output_filename = "final.parquet";
+		Path parquet_output_file_path = new Path(output_filename);
 
 		for (final File fileEntry : folder.listFiles()) {
 			if (fileEntry.isDirectory()) {
-				readWriteParquetMerge(fileEntry);
+				continue;
 			} else {
-				readWriteParquetMerge(fileEntry);
+				Path parquet_file_path = new Path(fileEntry.getPath());
+
+				ParquetReader<GenericRecord> reader = new AvroParquetReader<GenericRecord>(
+						parquet_file_path);
+
+				GenericRecord tmp = reader.read();
+				System.out.println("Schema: " + tmp.getSchema());
+
+				File fileptr = new File(output_filename);
+				if (!fileptr.exists()) {
+					writer = new AvroParquetWriter<GenericRecord>(
+							parquet_output_file_path, tmp.getSchema(),
+							CompressionCodecName.UNCOMPRESSED,
+							DEFAULT_BLOCK_SIZE, DEFAULT_PAGE_SIZE, false);
+				}
+				while ((tmp = reader.read()) != null) {
+					// System.out.println(tmp.toString());
+					writer.write(tmp);
+				}
+				reader.close();
 			}
 		}
-
-	}
-
-	private static void readWriteParquetMerge(File fileEntry)
-			throws IOException {
-		Path parquet_file_path = new Path(fileEntry.getPath());
-		String output_filename = "./final.parquet";
-		Path parquet_output_file_path = new Path(output_filename);
-
-		ParquetReader<GenericRecord> reader = new AvroParquetReader<GenericRecord>(
-				parquet_file_path);
-
-		GenericRecord tmp = reader.read();
-
-		System.out.println("Schema: " + tmp.getSchema());
-
-		ParquetWriter<GenericRecord> writer = new AvroParquetWriter<GenericRecord>(
-				parquet_output_file_path, tmp.getSchema(),
-				CompressionCodecName.UNCOMPRESSED, DEFAULT_BLOCK_SIZE,
-				DEFAULT_PAGE_SIZE, false);
-
-		while ((tmp = reader.read()) != null) {
-			System.out.println(tmp.toString());
-			writer.write(tmp);
-		}
-		reader.close();
 		writer.close();
 
 	}
