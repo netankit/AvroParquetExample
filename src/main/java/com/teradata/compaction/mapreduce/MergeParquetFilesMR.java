ea8b61998334f2e497b6590d2896cd827fa6c9dd
netankit <ankit.bahuguna@cs.tum.edu>
2014-09-03 14:48:10 +0200

    A working version of MergeParquetFilesMR.java, but requires tests to confirm!

--- src/main/java/com/teradata/compaction/mapreduce/MergeParquetFilesMR.java
+++ src/main/java/com/teradata/compaction/mapreduce/MergeParquetFilesMR.java
@@ -5,12 +5,13 @@ import java.io.IOException;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.IndexedRecord;
+import org.apache.avro.mapred.AvroValue;
+import org.apache.avro.mapreduce.AvroJob;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -32,27 +33,28 @@ public class MergeParquetFilesMR {
 	private static Schema fileSchema;
 
 	public static class SampleParquetMapper extends
-			Mapper<LongWritable, IndexedRecord, LongWritable, Text> {
+			Mapper<Void, IndexedRecord, NullWritable, AvroValue<IndexedRecord>> {
 		@Override
-		public void map(LongWritable key, IndexedRecord values, Context context)
+		public void map(Void key, IndexedRecord values, Context context)
 				throws IOException, InterruptedException {
-			Text mapvalues = new Text(values.toString());
-			// Gives: NullPointerException
-			context.write(new LongWritable(2), mapvalues);
-			// Gives: java.lang.ClassCastException:
-			// org.apache.hadoop.io.LongWritable
-			// cannot be cast to java.lang.Void
-			// context.write(null, mapvalues);
+
+			context.write(NullWritable.get(), new AvroValue<IndexedRecord>(
+					values));
 
 		}
 	}
 
-	public static class SampleParquetReducer extends
-			Reducer<LongWritable, Text, Void, Text> {
+	public static class SampleParquetReducer
+			extends
+			Reducer<NullWritable, AvroValue<IndexedRecord>, Void, IndexedRecord> {
 
-		public void reduce(LongWritable key, Text values, Context context)
+		@Override
+		public void reduce(NullWritable key,
+				Iterable<AvroValue<IndexedRecord>> records, Context context)
 				throws IOException, InterruptedException {
-			context.write(null, values);
+			for (AvroValue<IndexedRecord> avroValue : records) {
+				context.write(null, avroValue.datum());
+			}
 
 		}
 	}
@@ -61,24 +63,18 @@ public class MergeParquetFilesMR {
 		Configuration conf = new Configuration();
 		Job job = new Job(conf, "MergeParquet");
 		final Path inputPath = new Path(
-				"/home/ankit/workspace/AvroExample/newtest/");
+				"/home/ankit/workspace/AvroExample/newtest1/");
 		final Path out = new Path("output_newtest");
 		Schema schemaParquetFile = getBaseSchema(inputPath, conf);
 		job.setJarByClass(MergeParquetFilesMR.class);
 		job.setMapperClass(SampleParquetMapper.class);
 		job.setReducerClass(SampleParquetReducer.class);
-		// job.setOutputKeyClass(NullWritable.class);
-		// job.setOutputValueClass(IndexedRecord.class);
+
 		job.setInputFormatClass(AvroParquetInputFormat.class);
 		job.setOutputFormatClass(AvroParquetOutputFormat.class);
-		job.setOutputKeyClass(LongWritable.class);
-		// Set Schema for various phases
-		// AvroJob.setInputValueSchema(job, schemaParquetFile);
-		// AvroJob.setInputKeySchema(job, null);
-		// AvroJob.setMapOutputKeySchema(job, null);
-		// AvroJob.setMapOutputValueSchema(job, schemaParquetFile);
-		// AvroJob.setOutputKeySchema(job, null);
-		// AvroJob.setOutputValueSchema(job, schemaParquetFile);
+		job.setMapOutputKeyClass(NullWritable.class);
+		AvroJob.setMapOutputValueSchema(job, schemaParquetFile);
+
 		AvroParquetOutputFormat.setSchema(job, schemaParquetFile);
 		FileInputFormat.addInputPath(job, inputPath);
 		AvroParquetOutputFormat.setOutputPath(job, out);
